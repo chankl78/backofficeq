6d4c7bdc1eff2ae3e4b4e47709094012d368119c
Valery Krukov <valery.v.krukov@gmail.com>
2019-09-02 00:01:34 +0300

    User edit changes

--- src-spa/src/pages/dashboard/access/UserAccessEdit.vue
+++ src-spa/src/pages/dashboard/access/UserAccessEdit.vue
@@ -1,7 +1,10 @@
 <template>
     <q-page padding>
         <div class="q-pa-md q-gutter-sm" v-if="havePermissions">
-            <h4>Edit user</h4>
+            <h4>
+                Edit user
+                <q-btn icon-right="send" label="Reset password" @click="handleForgotPassword" class="float-right"/>
+            </h4>
             <q-form ref="roleForm" class="q-pa-md">
                 <div class="row">
                     <div class="col-lg-6 col-xs-12">
@@ -79,7 +82,7 @@
                             name="accessType"
                             v-model="accessType"
                             :options="typesList"
-                            label="Role"
+                            label="Access Type"
                             class="q-gutter-md q-mr-md"
                         >
                             <template v-slot:option="scope">
@@ -96,6 +99,34 @@
                         </q-select>
                     </div>
                 </div>
+                <q-markup-table separator="cell" flat bordered class="q-mt-md">
+                    <thead>
+                        <tr>
+                            <th colspan="2">
+                                <div class="row no-wrap items-center">
+                                    <h5>Active roles and access types</h5>
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- -->
+                    </tbody>
+                </q-markup-table>
+                <q-markup-table separator="cell" flat bordered class="q-mt-md">
+                    <thead>
+                        <tr>
+                            <th colspan="2">
+                                <div class="row no-wrap items-center">
+                                    <h5>Active modules</h5>
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- -->
+                    </tbody>
+                </q-markup-table>
                 <q-page-sticky position="bottom-right" :offset="[18, 18]">
                     <q-btn fab color="primary" icon="mdi-check" @click="save" class="q-mr-sm"/>
                     <q-btn fab to="/user-access" icon="mdi-cancel"/>
@@ -181,8 +212,39 @@ export default {
     })
   },
   methods: {
-    ...mapActions(['loadUser', 'updateUser']),
+    ...mapActions(['loadUser', 'updateUser', 'forgotPassword']),
     ...mapGetters(['currentUser', 'availableRoles', 'accessTypeList', 'availableStatuses']),
+    handleForgotPassword () {
+      let email = this.user.email
+      this.forgotPassword(email)
+        .then((resp) => {
+          this.$q.notify({
+            color: 'positive',
+            position: 'top',
+            message: 'Password reset link sent'
+          })
+        })
+        .catch((error) => {
+          if (error.response.status === 402) {
+            this.$q.notify({
+              color: 'negative',
+              position: 'top',
+              message: error.response.data.message,
+              icon: 'report_problem'
+            })
+          } else {
+            this.$q.notify({
+              color: 'negative',
+              position: 'top',
+              message: [
+                error.response.data.message,
+                error.response.data.errors.email
+              ].join('. '),
+              icon: 'report_problem'
+            })
+          }
+        })
+    },
     save () {
       this.updateUser({
         user: this.user,
